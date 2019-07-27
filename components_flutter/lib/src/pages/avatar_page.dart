0d27e14dff03eb6de78ec82344676def59f24b78
Lenin samaniego <leninsvg@gmail.com>
2019-07-27 17:46:33 -0500

    no message

--- components_flutter/lib/src/pages/avatar_page.dart
+++ components_flutter/lib/src/pages/avatar_page.dart
@@ -8,6 +8,29 @@ class AvatarPage extends StatelessWidget {
     return new Scaffold(
       appBar: new AppBar(
         title: Text('Avatar'),
+        actions: <Widget>[
+          Container(
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://pbs.twimg.com/media/CJj5n4QWUAAcLhQ.png'),
+              radius: 25.0,
+            ),
+            padding: EdgeInsets.all(5.0),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('LS'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://i.pinimg.com/474x/66/fc/b1/66fcb132daa68cd48fd42a233bc90efe.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
